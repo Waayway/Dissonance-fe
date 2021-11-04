784d7e65f179029b76a2a20f02ccd2af8fa9387c
Waayway <thijs@vanwaaij.com>
2021-11-04 12:31:04 +0100

    Added ChatDate, Which shows date of messages

---
+++ src/lib/MainArea/Chat/ChatDate.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let date;
+</script>
+
+<div class="w-full flex items-center my-2">
+    <hr class="w-full border-primary-light border-2"><p class='whitespace-nowrap mx-3'>{date}</p><hr class="w-full border-primary-light border-2">
+</div>
--- src/lib/MainArea/Chat/ChatField.svelte
+++ src/lib/MainArea/Chat/ChatField.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+import ChatDate from "./ChatDate.svelte";
+
     
 </script>
 
-<div class="h-full">
-
+<div class="h-full bg-primary-lighter">
+    <ChatDate date="November 19 2021" />
 </div>
