b040384e9d92ddc5546fe419ea1c0b8c3b475c1c
Waayway <thijs@vanwaaij.com>
2021-11-08 11:56:18 +0100

    Added ChatMessage, with text time and sender

--- src/lib/MainArea/Chat/ChatField.svelte
+++ src/lib/MainArea/Chat/ChatField.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 import ChatDate from "./ChatDate.svelte";
+import ChatMessage from "./ChatMessage.svelte";
+import SveltePng from '../../../assets/svelte.png'
 
     
 </script>
 
 <div class="h-full bg-primary-lighter">
     <ChatDate date="November 19 2021" />
+    <ChatMessage text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." time="09:16" sender="Waayway"><img src={SveltePng} alt=""></ChatMessage>
 </div>
---
+++ src/lib/MainArea/Chat/ChatMessage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let sender;
+    export let text;
+    export let time;
+</script>
+
+<div class="w-full grid p-2 main">
+    <div class="w-16 h-16 bg-primary-darker rounded-full">
+        <slot /> <!-- Icon Input -->
+    </div>
+    <div class="ml-2 flex flex-col h-auto p-2">
+        <p class="underline">{sender}</p>
+        <p>{text}</p>
+        <p class="text-xs">{time}</p>
+    </div>
+</div>
+
+<style>
+    .main {
+        grid-template-columns: 4rem calc(100% - 4rem);
+    }
+</style>
